3b577615028760271e645724dfe2ce24bfe9ae88
Jaden Gibbons <162081899+JadenG13@users.noreply.github.com>
2025-09-19 11:43:26 +1000

    Add launch argument for publishing rate configuration

--- launch/icm20948_node_launch.py
+++ launch/icm20948_node_launch.py
@@ -1,9 +1,16 @@
 from launch_ros.actions import Node
-
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch import LaunchDescription
 
 
 def generate_launch_description():
+    pub_rate_arg = DeclareLaunchArgument(
+        'pub_rate',
+        default_value='200',
+        description='Publishing rate in Hz'
+    )
+    
     return LaunchDescription(
         [
             Node(
@@ -13,7 +20,7 @@ def generate_launch_description():
                 parameters=[
                     {"i2c_address": 0x69},
                     {"frame_id": "imu_icm20948"},
-                    {"pub_rate": 50},
+                    {"pub_rate": LaunchConfiguration('pub_rate')},
                 ],
             )
         ]
